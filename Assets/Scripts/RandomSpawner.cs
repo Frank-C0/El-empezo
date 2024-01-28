c06a77f7e2327ebd0743ad00ae1b76890d68eced
Frank-C0 <fkanaco@unsa.edu.pe>
2024-01-27 23:01:18 -0500

    aleatory mechanic spawn fixed

--- Assets/Scripts/RandomSpawner.cs
+++ Assets/Scripts/RandomSpawner.cs
@@ -6,8 +6,9 @@ public class RandomSpawner : MonoBehaviour
 {
     public List<GameObject> prefabsToSpawn;
     public float respawnInterval;
+
+    public GameObject cubeKey;
     private bool canRespawn = true;
-    public float ejeX, ejeZ, ejeY;
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +22,12 @@ public class RandomSpawner : MonoBehaviour
         {
             if (canRespawn)
             {
-                //Vector3 randomSpawnPosition = new Vector3(Random.Range(15f, 35f), 13f, Random.Range(13f, 40f));
-                Vector3 randomSpawnPosition = new Vector3(ejeX, ejeY, ejeZ);
+                Vector3 randomSpawnPosition = new Vector3(Random.Range(-2, 2), 0, Random.Range(-2, 2));
+                Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z) + randomSpawnPosition;
                 GameObject selectedPrefab = prefabsToSpawn[Random.Range(0, prefabsToSpawn.Count)]; // Aleatoriedad de powerUp
-                Instantiate(selectedPrefab, randomSpawnPosition, Quaternion.identity); // Instancias ese objeto
+                PowerUp instanceObj = Instantiate(selectedPrefab, spawnPosition, Quaternion.identity).GetComponent<PowerUp>(); // Instancias ese objeto
+                instanceObj.createCube(cubeKey, Random.Range(-4, 4) + transform.position.y);
+
                 canRespawn = false;
                 yield return new WaitForSeconds(respawnInterval);
                 canRespawn = true;
